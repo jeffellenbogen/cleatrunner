96157c488222b094c5d72b1ad356b2c5dbbc40c6
jeffellenbogen <jeffellenbogen@users.noreply.github.com>
2024-11-11 14:31:09 -0700

    Update LED_sandbox.py
    fixed to correct pin and added microbit import *

--- LED_sandbox.py
+++ LED_sandbox.py
@@ -1,11 +1,12 @@
 # This is for learning about neoPixel control on a micro:bit using python
+from microbit import *
 import neopixel
 numLEDs=80
 color1=(255,0,0)
 color2=(0,255,0)
 color3=(0,0,255)
 
-np = neopixel.NeoPixel(pin0, numLEDs)
+np = neopixel.NeoPixel(pin2, numLEDs)
 
 for index in range(numLEDs):
 	if (index%3 == 0):
