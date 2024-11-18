5fd513b0da0d8acc8a1da86765fd54232fe31c66
jeffellenbogen <jeffellenbogen@users.noreply.github.com>
2024-11-18 15:51:43 -0700

    Update LED_sandbox-gradient.py
    testing all 3 strips

--- LED_sandbox-gradient.py
+++ LED_sandbox-gradient.py
@@ -1,25 +1,30 @@
 # This is for learning about neoPixel control on a micro:bit using python
 from microbit import *
 import neoPixelPlus
+numLEDs0=80
 numLEDs1=156
 numLEDs2=136
+
 
 red=(255,0,0)
 green=(0,255,0)
 blue=(0,0,255)
 
-np0 = neoPixelPlus.neoPixelPlus(pin0, numLEDs1)
-np1 = neoPixelPlus.neoPixelPlus(pin1, numLEDs2)
-
+np0 = neoPixelPlus.neoPixelPlus(pin2, numLEDs0)
+np1 = neoPixelPlus.neoPixelPlus(pin1, numLEDs1)
+np2 = neoPixelPlus.neoPixelPlus(pin0, numLEDs2)
 
 np0.gradient(red,green)
 np1.gradient(blue,red)
+np2.gradient(green,blue)
 
 
 while True:
 	np0.rotate(-1)
-	#np1.rotate(1)
+	np1.rotate(1)
+	np2.rotate(1)
 	np0.show()
-	#np1.show()
+	np1.show()
+	np2.show()
 
 
