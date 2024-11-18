22ff09e075036a31a003113eadd2928a3c2b6228
jeffellenbogen <jeffellenbogen@users.noreply.github.com>
2024-11-18 14:28:51 -0700

    broke out test cases into individual py files

---
+++ LED_sandbox-rotate.py
@@ -0,0 +1,27 @@
+# This is for learning about neoPixel control on a micro:bit using python
+from microbit import *
+import neoPixelPlus
+numLEDs1=81
+numLEDs2=120
+
+red=(255,0,0)
+green=(0,255,0)
+blue=(0,0,255)
+
+np0 = neoPixelPlus.neoPixelPlus(pin2, numLEDs1)
+np1 = neoPixelPlus.neoPixelPlus(pin1, numLEDs2)
+
+np0.fill(red)
+np0[0]=blue
+
+np1.fill(blue)
+np1[0]=red
+
+
+while True:
+	np0.rotate(-1)
+	np1.rotate(1)
+	np0.show()
+	np1.show()
+
+
