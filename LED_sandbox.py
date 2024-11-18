38e433cb760afa612d68cab830a3c85649d36c33
jeffellenbogen <jeffellenbogen@users.noreply.github.com>
2024-11-18 13:45:01 -0700

    Update LED_sandbox.py
    update with new neoPixelPlus library and reformat test strips.

--- LED_sandbox.py
+++ LED_sandbox.py
@@ -1,39 +1,27 @@
 # This is for learning about neoPixel control on a micro:bit using python
 from microbit import *
-import neopixel
+import neoPixelPlus
 numLEDs1=81
 numLEDs2=120
 
-color1=(255,0,0)
-color2=(0,255,0)
-color3=(0,0,255)
+red=(255,0,0)
+green=(0,255,0)
+blue=(0,0,255)
 
-np = neopixel.NeoPixel(pin2, numLEDs1)
-np2 = neopixel.NeoPixel(pin1, numLEDs2)
+np0 = neoPixelPlus.neoPixelPlus(pin2, numLEDs1)
+np1 = neoPixelPlus.neoPixelPlus(pin1, numLEDs2)
 
-for index in range(numLEDs1):
-	if (index%3 == 0):
-		np[index]=color1
-	elif (index%3 == 1):
-		np[index]=color2
-	elif (index%3 == 2):
-		np[index]=color3
+np0.fill(red)
+np0[0]=blue
 
-np.show()
-
-
-def rotate(strip, direction):
-	length = len(strip)
-	# Rotate pixels left
-	if (direction == -1):
-		tempPixelData = strip[0]
-		for index in range(length-1):
-			strip[index]=strip[index+1]
-		strip[length-1]=tempPixelData	
+np1.fill(blue)
+np1[0]=red
 
 
 while True:
-	rotate(np,-1)
-	np.show()
+	rotate(np0,-1)
+	rotate(np1,1)
+	np0.show()
+	np1.show()
 
 
